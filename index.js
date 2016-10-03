391fcd64f48f6a0d8aa539ba7daf044468040a0d
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2016-10-03 14:48:53 +0330

    Clearer index code with bonny-cli-renderer

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-const child_process = require('child_process');
 const fs = require('fs');
 const cli = require('bonny-cli-renderer');
-
-const exec = child_process.exec;
-const spawn = child_process.spawn;
 
 class BonnyLaravel {
 
@@ -18,41 +14,23 @@ class BonnyLaravel {
         fs.accessSync("./vendor/", fs.F_OK);
         that.artisanServe()
     } catch (e) {
-      cli.log(`
-        <message>unzipping the vendor.zip...</message>
-      `);
-      exec('unzip vendor.zip', {
-        encoding: 'utf8',
-        maxBuffer: 1000*1024
-      }, (error, stdout, stderr) => {
-          if (error) {
-            cli.log(`<error> ${error} </error>`);
-            return;
-          }
-          cli.log(`<success>unzipping completed </success>`);
-          that.artisanServe();
+      cli.log(`<message>unzipping the vendor.zip...</message>`);
+      cli.exec('unzip vendor.zip', ()=>{
+        cli.log(`<success> unzipping vendor.zip completed</success>`);
+        that.artisanServe();
       });
     }
   }
 
   artisanServe() {
-
     this.artisanGenerateKey();
-
-    const serve = spawn('php', ['artisan', 'serve']);
-
-    serve.stdout.on('data', (data) => {
-      cli.log(`<message>${data}</message>`);
-    });
-
-    serve.stderr.on('data', (data) => {
-      cli.log(`<error> ${data} </error>`);
-    });
+    cli.runtimeExec('php artisan serve');
   }
 
   artisanGenerateKey() {
-    exec('php artisan key:generate');
-    cli.log(`<success>New Security Key Generated! 🔐</success>`);
+    cli.exec('php artisan key:generate', ()=>{
+      cli.log(`<success>New Security Key Generated! 🔐</success>`);
+    });
   }
 
 }
